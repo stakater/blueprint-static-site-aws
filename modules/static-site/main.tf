01e0480ff22499cd980140db7d8d76bf7ddfe6f5
ahsan-storm <ahsanmuhammad1@outlook.com>
2018-11-05 01:00:55 +0500

    adds cloudfront and route53 modules

--- modules/static-site/main.tf
+++ modules/static-site/main.tf
@@ -11,3 +11,39 @@ module "s3bucket_gocarbook" {
   redirect_all_requests_to = "${module.s3bucket_www_gocarbook.bucket_name}"
 }
 
+module "cloudfront_gocarbook" {
+    source = "github.com/stakater/blueprint-utilities-aws.git//modules/cloudfront"
+    bucket-domain-name = "${module.s3bucket_www_gocarbook.bucket_domain_name}"
+    bucket-id = "${module.s3bucket_www_gocarbook.bucket_name}"
+    enabled = "${var.cloudfront_enabled}"
+    ipv6-enabled= "${var.cloudfront_ipv6_enabled}"
+    default-root-object = "${var.cloudfront_default_root_object}"
+    domain-aliases = "${var.cloudfront_domain_aliases}"
+    viewer-protocol-policy = "${var.cloudfront_viewer_protocol_policy}"
+    acm-certificate-arn = "${var.cloudfront_acm_certificate_arn}"
+}
+
+module "route53_hostedzone_gocarbook" {
+    source = "github.com/stakater/blueprint-utilities-aws.git//modules/route53/public"
+    public_domain = "${module.s3bucket_gocarbook.bucket_name}"
+}
+
+module "route53_record_gocarbook" {
+  source = "github.com/stakater/blueprint-utilities-aws.git//modules/route53/record"
+  zone_id = "${module.route53_hostedzone_gocarbook.zone_id}"
+  name = "${module.s3bucket_gocarbook.bucket_name}"
+  type = "${var.route53_record_gocarbook_type}"
+  alias_name = "${module.s3bucket_gocarbook.website_domain}"
+  alias_zone_id = "${module.s3bucket_gocarbook.hosted_zone_id}"
+  target_health = "${var.route53_record_gocarbook_target_health}"
+}
+
+module "route53_record_www_gocarbook" {
+  source = "github.com/stakater/blueprint-utilities-aws.git//modules/route53/record"
+  zone_id = "${module.route53_hostedzone_gocarbook.zone_id}"
+  name = "${module.s3bucket_www_gocarbook.bucket_name}"
+  type = "${var.route53_record_www_gocarbook_type}"
+  alias_name = "${module.cloudfront_gocarbook.cloudfront_domain_name}"
+  alias_zone_id = "${module.cloudfront_gocarbook.hosted_zone_id}"
+  target_health = "${var.route53_record_www_gocarbook_target_health}"
+}
--- modules/static-site/variable.tf
+++ modules/static-site/variable.tf
@@ -6,10 +6,6 @@ variable "bucket_gocarbook_acl" {
     default = ""
 }
 
-variable "bucket_gocarbook_redirect_all_requests_to" {
-    default = ""
-}
-
 variable "bucket_wwwgocarbook_name" {
     default = ""
 }
@@ -20,4 +16,47 @@ variable "bucket_wwwgocarbook_acl" {
 
 variable "bucket_wwwgocarbook_policy" {
     default = ""
+}
+
+variable "cloudfront_enabled" {
+  default = true
+}
+
+variable "cloudfront_ipv6_enabled" {
+    default = true
+}
+
+variable "cloudfront_default_root_object" {
+    type = "string"
+    default = ""
+}
+
+variable "cloudfront_domain_aliases" {
+    type = "list"
+}
+
+variable "cloudfront_viewer_protocol_policy" {
+    type = "string"
+    default = "allow-all"
+}
+
+variable "cloudfront_acm_certificate_arn" {
+    type = "string"
+    default = ""
+}
+
+variable "route53_record_gocarbook_type" {
+    default = "A"
+}
+
+variable "route53_record_gocarbook_target_health" {
+    default = "true"
+}
+
+variable "route53_record_www_gocarbook_type" {
+    default = "A"
+}
+
+variable "route53_record_www_gocarbook_target_health" {
+    default = "true"
 }
