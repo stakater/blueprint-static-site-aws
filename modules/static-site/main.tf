403ca4c86dbad2376a24c26639ed0222bcc4de37
ahsan-storm <ahsanmuhammad1@outlook.com>
2018-11-05 00:33:44 +0500

    fix gocarbook bucket source

--- modules/static-site/main.tf
+++ modules/static-site/main.tf
@@ -5,7 +5,7 @@ module "s3bucket_www_gocarbook" {
   policy = "${var.bucket_wwwgocarbook_policy}"
 }
 module "s3bucket_gocarbook" {
-  source = "github.com/stakater/blueprint-storage-aws.git//modules/s3/public"
+  source = "github.com/stakater/blueprint-storage-aws.git//modules/s3/public/redirect"
   name = "${var.bucket_gocarbook_name}"
   acl = "${var.bucket_gocarbook_acl}"
   redirect_all_requests_to = "${module.s3bucket_www_gocarbook.bucket_name}"
